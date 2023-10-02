e86c03e1009345017a31233c26cbee647bfdf530
Ismail Mohammed Ali <ishyali1@outlook.com>
2023-10-02 20:56:23 +0100

    first commit

---
+++ challenge.js
@@ -0,0 +1,37 @@
+for(let i = 0; i < 20; i++) {
+    console.log("Ismail")
+}
+
+
+
+let i = 0;
+while (i < 20) {
+    console.log("Ali");
+    i++;
+}
+
+let today = "tuesday"
+if (today == "monday") {
+console.log("it is coding day")
+ } else if (today == "tuesday"){
+    console.log("its my off day")
+ }else {
+    console.log("ask me personally which day")
+ }
+
+switch (today) {
+    case "monday":
+        console.log("it is coding day, from switch")
+        break;
+    case "tuesday":
+        console.log("it is my off day, from switch")
+        break;
+
+    default:
+        console.log("ask me personally which day, from switch")
+        break;
+}
+
+
+
+
